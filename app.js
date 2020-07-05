650d523ac944db065da0a690e5456d10ad5d5232
Lisan <lisannekraal@gmail.com>
2020-07-05 21:37:23 +0200

    updated form and post route

--- app.js
+++ app.js
@@ -22,9 +22,37 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+//FORM TO MAKE A NEW CATCALL
 app.get("/new", function(req, res){
     res.render("new");
 });
+
+//CREATE A NEW CATCALL/POST REQUEST
+app.post("/", function(req, res){
+    //retrieve information from form and save as object
+    const newCatcall = {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: req.body.coordinates
+        },
+        properties: {
+            description: req.body.description,
+            date: req.body.date,
+            context: req.body.context
+        }
+    }
+
+    //create new Catcall and save to db
+    Catcall.create(newCatcall, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //req.flash("success", "");
+            res.redirect("home");
+        }
+    });
+})
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Server started");
