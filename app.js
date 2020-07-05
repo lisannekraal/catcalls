33769c5909905d41742dfc9d09c00081d79b70ea
Lisan <lisannekraal@gmail.com>
2020-07-05 21:20:52 +0200

    try again

--- app.js
+++ app.js
@@ -8,18 +8,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const url = 'mongodb+srv://ModeratorCatcalls:OVqZGJACRvAZuiui@cluster0.h0tqu.mongodb.net/catcall?retryWrites=true&w=majority';
-//const url = process.env.DATABASEURL;
+// const url = 'mongodb+srv://ModeratorCatcalls:OVqZGJACRvAZuiui@cluster0.h0tqu.mongodb.net/catcall?retryWrites=true&w=majority';
+const url = process.env.DATABASEURL;
 //ModeratorCatcalls OVqZGJACRvAZuiui
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //made model, not tested yet
 //try to insert something using the catcall model
 //to the atlasDB, which is now linked I think
-
-
-
-
 
 
 app.get("/", function(req, res){
